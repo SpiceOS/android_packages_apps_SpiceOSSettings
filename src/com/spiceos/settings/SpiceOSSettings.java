4fee1a6cf756ba8e15c8e16e9b2aeb9c6f510572
jijinraj <jijinraj401@gmail.com>
2022-05-31 10:08:41 +0530

    replicated @ProjectBlaze settings home page implementation on our Brewery (picked from : https://github.com/ProjectBlaze/packages_apps_Settings/commit/28602715e905bb0548b99ead18da86ba060a4877)
    - added accent based colors for icons
    (picked from : https://github.com/SpiceOS-Beta/android_packages_apps_Settings/commit/7d9dceadb74d678989317ab6a51648f4bbd7deff)
    
    all the credits goes to respective authors...
    Peace out
    
    Co-authored-by: Jason Chiu <chiujason@google.com>
    Co-authored-by: beingmishra <rahul731999@gmail.com>
    Co-authored-by: dlwlrma123 <alexfinhart@gmail.com>
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>
    Signed-off-by: jijinraj <jijinraj401@gmail.com>

--- src/com/spiceos/settings/SpiceOSSettings.java
+++ src/com/spiceos/settings/SpiceOSSettings.java
@@ -16,26 +16,71 @@
 
 package com.spiceos.settings;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceFragmentCompat;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 import com.android.internal.logging.nano.MetricsProto;
 
 public class SpiceOSSettings extends SettingsPreferenceFragment {
 
-    @Override
-    public void onCreate(Bundle icicle) {
+     @Override
+     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.spiceos_settings);
+        final PreferenceScreen screen = getPreferenceScreen();
+        if (screen == null) {
+            return;
+        }
+
+        final int tintColor = Utils.getHomepageIconColor(getContext());
+        final int count = screen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            final Preference preference = screen.getPreference(i);
+            if (preference == null) {
+                break;
+            }
+            final Drawable icon = preference.getIcon();
+            if (icon != null) {
+                icon.setTint(tintColor);
+            }
+        }
+        onSetPrefCard();
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.SPICEOS_SETTINGS;
     }
+
+        private void onSetPrefCard() {
+    	final PreferenceScreen screen = getPreferenceScreen();
+        final int count = screen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            final Preference preference = screen.getPreference(i);
+            if (screen == null) {
+                return;
+            }
+ 	    String key = preference.getKey();
+
+        if (key.equals("card_top")){
+	        preference.setLayoutResource(R.layout.custom_level_preference_top);
+	    }
+        if (key.equals("card_middle")){
+	        preference.setLayoutResource(R.layout.card_view_pref_middle);
+	    }
+        if (key.equals("card_bottom")){
+	        preference.setLayoutResource(R.layout.card_view_pref_bottom);
+	    }
+
+     	}
+    }
+
 }
